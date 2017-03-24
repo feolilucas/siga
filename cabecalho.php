9c403070746e5da2c2a4e23f96e02a6fcfe7cf83
feolilucas <feoli.lucas@gmail.com>
2017-03-24 10:37:34 -0300

    Favicon
    Favicon

--- cabecalho.php
+++ cabecalho.php
@@ -1,11 +1,15 @@
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/boxshadow.css">
-  <link rel="stylesheet" href="css/shadow.css">
-  <link rel="stylesheet" href="css/csspersonalizado.css">
-  <script src="js/jquery-2.1.3.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/validator.min.js"></script>
-  <script src="js/funcoes.js"></script>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/boxshadow.css">
+	<link rel="stylesheet" href="css/shadow.css">
+	<link rel="stylesheet" href="css/csspersonalizado.css">
+	<script src="js/jquery-2.1.3.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/validator.min.js"></script>
+	<script src="js/funcoes.js"></script>
 </head>
