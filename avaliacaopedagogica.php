e3c9ca217c52a9f547b42ae82caf1637bc091e02
feolilucas <feoli.lucas@gmail.com>
2017-03-24 12:11:00 -0300

    Tela Avaliação Pedagogica
    Tela Avaliação Pedagogica

---
+++ avaliacaopedagogica.php
@@ -0,0 +1,98 @@
+<html>
+
+<?php  
+/*include "session.php";*/
+include "cabecalho.php";
+include "menu.php";
+
+require_once "classearea.php";
+$area = new area;
+$r = $area->buscarAreas();
+?>
+<title>Cadastro de Alunos</title>
+
+<body style="padding-top:60px;">
+  <section style="width:60%" class="container">
+
+    <!-- Modal -->
+    <div class="modal fade in" id="divSucesso" role="dialog" style="display:none; position: absolute;top: 20%;">
+      <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Cadastro realizado</h4>
+          </div>
+          <div class="modal-body">
+            <p>Aluno cadastrado com sucesso.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal" onclick="location.href='http://localhost/siga/cadastroaluno.php';">Fechar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs " role="tablist">
+        <li role="presentation" class="active"><a href="#avaliacaopedagogica" aria-controls="avaliacaopedagogica" role="tab" data-toggle="tab">Avaliação Pedagógica</a></li>
+
+
+      </ul>
+
+      <!-- Tab panes -->
+
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active form-group panel panel-body" id="avaliacaopedagogica" style="border-color: #00688B;">
+          <div style="padding-top:20px;">
+
+            <form class="form-horizontal" action="" method="POST" data-toggle="validator">
+
+              <div class="form-group panel panel-body">
+
+                <fieldset>
+
+                  <div style="padding-left: 5%; padding-right: 5%;">
+
+
+
+                    <div class="row">
+                      <div class="col-md-12 form-group">
+                        <label for="avaliacaopedagogica" class="control-label">Avaliação Pedagógica</label>
+                        <textarea rows="7" id="avaliacaopedagogica" name="avaliacaopedagogica" class="form-control input-md" required></textarea>
+                      </div>
+                    </div>
+
+
+                    <div class="row">
+                      <div class="col-md-12 form-group">
+                        <label for="observacoesgerais" class="control-label">Observações Gerais</label>
+                        <textarea rows="4" id="observacoesgerais" name="observacoesgerais" class="form-control input-md" required></textarea>
+                      </div>
+                    </div>
+
+
+                  </div>
+
+                </fieldset>
+
+
+
+                <!-- Button (Double) -->
+                <br><br><br>
+                <div class="form-group">
+                  <label class="col-md-4 control-label" for="idConfirmar"></label>
+                  <div class="col-md-8">
+                    <button id="idConfirmar" name="idConfirmar" class="btn btn-success" type="submit">Confirmar</button>
+                    <button id="idCancelar" name="idCancelar" class="btn btn-danger">Cancelar</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
--- cadastroaluno.php
+++ cadastroaluno.php
@@ -9,7 +9,7 @@ require_once "classearea.php";
 $area = new area;
 $r = $area->buscarAreas();
 ?>
- <title>Cadastro de Alunos</title>
+<title>Cadastro de Alunos</title>
 
 <body style="padding-top:60px;">
   <section style="width:60%" class="container">
@@ -68,24 +68,24 @@ $r = $area->buscarAreas();
 
 
                     <div class="row">
-                    
+
                       <div class="col-md-6 form-group">
                         <label for="rg" class="control-label">RG</label>
                         <input id="rg" type="text" maxlength="10" size="19" name="rg" class="form-control input-md" onkeyup="somenteNumeros(this);">
                         <span class="help-block">Somente números</span> 
                       </div>
-                        <div class="col-md-6 form-group">
+                      <div class="col-md-6 form-group">
                         <label for="cpf" class="control-label">CPF</label>
                         <input type="text" name="cpf" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" class="form-control input-md" id="cpf">
                       </div>
-                      </div>
+                    </div>
 
-                      <div class="row">
+                    <div class="row">
                       <div class="col-md-4 form-group">
                         <label for="dataemissaorg" class="control-label">Data de emissão do RG</label>
                         <input id="dataemissaorg" type="date" maxlength="10" size="19" name="dataemissaorg" class="form-control input-md">
-                       </div>
-                       </div>        
+                      </div>
+                    </div>        
 
 
                     <div class="row">
@@ -109,100 +109,99 @@ $r = $area->buscarAreas();
                         <textarea rows="5" id="deficiencia" name="deficiencia" class="form-control input-md" required>Informe a deficiência do aluno...</textarea>
                       </div>
                     </div>
-</div></fieldset>
+                  </div></fieldset>
 
 
-                   
-                   
-                <!-- Button (Double) -->
-                <br><br><br>
-                <div class="form-group">
-                  <label class="col-md-4 control-label" for="idConfirmar"></label>
-                  <div class="col-md-8">
-                    <button id="idConfirmar" name="idConfirmar" class="btn btn-success" type="submit">Confirmar</button>
-                    <button id="idCancelar" name="idCancelar" class="btn btn-danger">Cancelar</button>
+
+
+                  <!-- Button (Double) -->
+                  <br><br><br>
+                  <div class="form-group">
+                    <label class="col-md-4 control-label" for="idConfirmar"></label>
+                    <div class="col-md-8">
+                      <button id="idConfirmar" name="idConfirmar" class="btn btn-success" type="submit">Confirmar</button>
+                      <button id="idCancelar" name="idCancelar" class="btn btn-danger">Cancelar</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
 
 
 
-        <div role="tabpanel" class="tab-pane form-group panel panel-body" id="endereco" style="border-color: #00688B;">
-          <div style="padding-top:20px;">
+          <div role="tabpanel" class="tab-pane form-group panel panel-body" id="endereco" style="border-color: #00688B;">
+            <div style="padding-top:20px;">
 
-            <form class="form-horizontal" action="" method="POST" data-toggle="validator">
+              <form class="form-horizontal" action="" method="POST" data-toggle="validator">
 
-              <div class="form-group panel panel-body">
+                <div class="form-group panel panel-body">
 
-                <fieldset>
-                  <div style="padding-left: 5%; padding-right: 5%;" width: "80%;">
-                    <div class="row">
-                      <div class="col-md-4 form-group">
-                        <label for="cep" class="control-label">CEP</label>
-                        <input name="cep" type="text" id="cep" value="" OnKeyPress="formatar('#####-###', this)" size="10" maxlength="9"  class="form-control input-md" required>
+                  <fieldset>
+                    <div style="padding-left: 5%; padding-right: 5%;" width: "80%;">
+                      <div class="row">
+                        <div class="col-md-4 form-group">
+                          <label for="cep" class="control-label">CEP</label>
+                          <input name="cep" type="text" id="cep" value="" OnKeyPress="formatar('#####-###', this)" size="10" maxlength="9"  class="form-control input-md" required>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-xs-12 col-md-10 form-group">
+                          <label for="logradouro" class="control-label">Logradouro</label>
+                          <input type="text" size="50" maxlength="300" id="logradouro" name="logradouro"  class="form-control input-md"required>
+                        </div>
+                        <div class="col-xs-6 col-md-2 form-group">
+                          <label for="numero" class="control-label">Número</label>
+                          <input type="text" maxlength="20" size="5" id="numero" name="numero"  class="form-control input-md" required>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-8 form-group">
+                          <label for="bairro" class="control-label">Bairro</label>
+                          <input type="text" size="58" id="bairro" maxlength="300" name="bairro" class="form-control input-md" required>
+                        </div>
+                        <div class="col-md-4 form-group">
+                          <label for="cidade" class="control-label">Cidade</label>
+                          <input type="text" id="cidade" maxlength="300" name="cidade" class="form-control input-md" required>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-2 form-group">
+                          <label for="estado" class="control-label">Estado</label>
+                          <input type="text" id="estado" maxlength="2" name="estado" class="form-control input-md" required>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12 form-group">
+                          <label for="complemento" class="control-label">Complemento</label>
+                          <input type="text" maxlength="300" name="complemento" class="form-control input-md">
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12 form-group">
+                          <label for="referencia" class="control-label">Referência</label>
+                          <input type="text" maxlength="300" name="referencia"  class="form-control input-md">
+                        </div>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="col-xs-12 col-md-10 form-group">
-                        <label for="logradouro" class="control-label">Logradouro</label>
-                        <input type="text" size="50" maxlength="300" id="logradouro" name="logradouro"  class="form-control input-md"required>
-                      </div>
-                      <div class="col-xs-6 col-md-2 form-group">
-                        <label for="numero" class="control-label">Número</label>
-                        <input type="text" maxlength="20" size="5" id="numero" name="numero"  class="form-control input-md" required>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-8 form-group">
-                        <label for="bairro" class="control-label">Bairro</label>
-                        <input type="text" size="58" id="bairro" maxlength="300" name="bairro" class="form-control input-md" required>
-                      </div>
-                      <div class="col-md-4 form-group">
-                        <label for="cidade" class="control-label">Cidade</label>
-                        <input type="text" id="cidade" maxlength="300" name="cidade" class="form-control input-md" required>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-2 form-group">
-                        <label for="estado" class="control-label">Estado</label>
-                        <input type="text" id="estado" maxlength="2" name="estado" class="form-control input-md" required>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-12 form-group">
-                        <label for="complemento" class="control-label">Complemento</label>
-                        <input type="text" maxlength="300" name="complemento" class="form-control input-md">
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-12 form-group">
-                        <label for="referencia" class="control-label">Referência</label>
-                        <input type="text" maxlength="300" name="referencia"  class="form-control input-md">
-                      </div>
-                    </div>
-                  </div>
-                </fieldset>
+                  </fieldset>
 
-                <!-- Button (Double) -->
-                <br><br><br>
-                <div class="form-group">
-                  <label class="col-md-4 control-label" for="idConfirmar"></label>
-                  <div class="col-md-8">
-                    <button id="idConfirmar" name="idConfirmar" class="btn btn-success" type="submit">Confirmar</button>
-                    <button id="idCancelar" name="idCancelar" class="btn btn-danger">Cancelar</button>
+                  <!-- Button (Double) -->
+                  <br><br><br>
+                  <div class="form-group">
+                    <label class="col-md-4 control-label" for="idConfirmar"></label>
+                    <div class="col-md-8">
+                      <button id="idConfirmar" name="idConfirmar" class="btn btn-success" type="submit">Confirmar</button>
+                      <button id="idCancelar" name="idCancelar" class="btn btn-danger">Cancelar</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
 
 
 
-       
 
 
 
