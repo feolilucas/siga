71a1f8e70aeee435579f6a49a2f820d29f2c327e
feolilucas <feoli.lucas@gmail.com>
2017-03-24 10:55:56 -0300

    Tamanho da fonte
    Alterado CSS para consertar o tamanho da fonte.

--- cabecalho.php
+++ cabecalho.php
@@ -3,11 +3,12 @@
 
 <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 
-
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/boxshadow.css">
 	<link rel="stylesheet" href="css/shadow.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/csspersonalizado.css">
+	
+
 	<script src="js/jquery-2.1.3.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/validator.min.js"></script>
